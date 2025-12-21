ff0abae8a0108c9bd0f47f21a3114a253c806db2
Mahmoud Elshabrawy <mahmoudwaell391@gmail.com>
2025-12-21 05:06:34 +0200

    implement validation pipe and custom validation

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsString, Length } from "class-validator"
+
 export class CreateUserDto {
+    @IsString()
+    @Length(3, 20)
     name: string
+
+    @IsEmail()
     email: string
+
+    @IsString()
     country: string
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, NotFoundException, Param, ParseUUIDPipe, Patch, Post } from "@nestjs/common";
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { UpdateUserDto } from "./dtos/update-user.dto";
 import { UserEntity } from "./user.entity";
 import { v4 as uuid } from 'uuid'
+// import { CustomValidationPipe } from "./pipes/validation.pipe";
 
 @Controller('users')
 export class UsersController {
@@ -14,7 +15,9 @@ export class UsersController {
     }
 
     @Get(':id')
-    findOne(@Param('id') id: string): UserEntity | undefined {
+    findOne(@Param('id', ParseUUIDPipe) id: string): UserEntity | undefined {
+        console.log(typeof (id));
+
         return this.users.find(user => user.id === id)
     }
 
@@ -26,16 +29,19 @@ export class UsersController {
     }
 
     @Patch(":id")
-    update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    update(@Param('id', ParseUUIDPipe) id: string, @Body() updateUserDto: UpdateUserDto) {
         // find user based on id
         const index = this.users.findIndex(user => user.id.toString() === id)
+        if (index === -1) {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
         this.users[index] = { ...this.users[index], ...updateUserDto }
         return this.users[index]
     }
 
     @Delete(':id')
     @HttpCode(204)
-    delete(@Param('id') id: string) {
+    delete(@Param('id', ParseUUIDPipe) id: string) {
         const index = this.users.findIndex(user => user.id.toString() === id)
         this.users.splice(index, 1)
         return null
