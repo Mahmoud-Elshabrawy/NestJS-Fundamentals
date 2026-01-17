bc759f31dd58427edfefe9a9c0dee39ab46f4c0a
Mahmoud Elshabrawy <mahmoudwaell391@gmail.com>
2026-01-17 22:19:06 +0200

    implement LoggerMiddleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
-import { ClassSerializerInterceptor, Module } from "@nestjs/common";
+import { ClassSerializerInterceptor, MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
 import { UserModule } from "./users/users.module";
 import { CommonModule } from './common/common.module';
+import { LoggerMiddleware } from "./common/middlewares/logger.middleware";
+import { UsersController } from "./users/users.controller";
 
 
 @Module({
@@ -11,6 +13,13 @@ import { CommonModule } from './common/common.module';
         },
     ]
 })
-export class AppModule {
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        // throw new Error("Method not implemented.");
+        consumer.apply(LoggerMiddleware).exclude(
+            { path: 'users/:id', method: RequestMethod.PATCH },
+            { path: 'users/:id', method: RequestMethod.DELETE }
+        ).forRoutes(UsersController)
+    }
 
 }
